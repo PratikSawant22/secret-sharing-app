6d100c80e6956983b10e9e4fcb95f2330352f3d7
Pratik Sawant <pratiksawant22619@gmail.com>
2021-05-13 21:07:55 +0530

    updating port

--- app.js
+++ app.js
@@ -169,6 +169,6 @@ if (port == null || port ==""){
     port = 3000;
 }
 
-app.listen(port,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server has started on :" +port );
 });
